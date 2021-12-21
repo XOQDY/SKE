b0997c77544812d130325c807268179662d11502
XOQDY <numfahmaek@gmail.com>
2021-12-21 19:13:01 +0700

    :zap: use vector instead Node :zap:

--- Algo/A row of fortune cookies/A row of fortune cookies.cpp
+++ Algo/A row of fortune cookies/A row of fortune cookies.cpp
@@ -1,127 +1,33 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
-
-typedef int ValueType;
-struct ListNode{
-    ValueType val;
-    ListNode* next;
-
-    ListNode(ValueType v, ListNode* nxt = nullptr)
-            : val(v), next(nxt) {}
-};
-
-ListNode* first = nullptr;
-ListNode* last = nullptr;
-
-int cookie_count = 0;
-
-void print_list(ListNode*& first)
-{
-    ListNode* temp = first;
-    do
-    {
-        cout << temp->val << endl;
-        temp = temp->next;
-    } while (temp != nullptr);
-}
-
-void insert_end(ListNode*& first, ListNode*& last, int val)
-{
-    ListNode* n = new ListNode(val);
-    if (first == nullptr)
-    {
-        first = n;
-    } else if (last == first)
-    {
-        first->next = n;
-    } else
-    {
-        last->next = n;
-    }
-    last = n;
-}
-
-void insert(ListNode*& first, int val, int loc)
-{
-    ListNode* n = new ListNode(val);
-    ListNode* temp = first;
-
-    int i = 0;
-    while (i != loc)
-    {
-        if (i == 0)
-        {
-            ++i;
-            continue;
-        }
-
-        temp = temp->next;
-        ++i;
-    }
-
-    if (i == 0)
-    {
-        n->next = first;
-        first = n;
-    } else
-    {
-        n->next = temp->next;
-        temp->next = n;
-    }
-}
-
-void eat(ListNode*& first, int loc)
-{
-    ListNode* temp = first;
-    ListNode* keep = first->next;
-
-    int i = 1;
-    while (i != loc)
-    {
-        if (i == 1)
-        {
-            ++i; continue;
-        }
-        temp = temp->next;
-        ++i;
-    }
-    if (i == 1)
-    {
-        delete first;
-        first = keep;
-    } else
-    {
-        keep = temp->next->next;
-        delete temp->next;
-        temp->next = keep;
-    }
-}
 
 int main()
 {
     int t;
     cin >> t;
 
+    vector<int> row;
+
     while (t--)
     {
         int l, k;
         cin >> l >> k;
 
-        if (l==1){
+        if (l == 1)
+        {
             int x;
             cin >> x;
-            if (k < cookie_count)
-                insert(first, x, k);
+
+            if (k <= row.size())
+                row.insert(row.begin() + k, x);
             else
-                insert_end(first, last, x);
-            ++cookie_count;
-        } else{
-            if (k <= cookie_count)
-            {
-                eat(first, k);
-                --cookie_count;
-            }
-        }
+                row.push_back(x);
+        } else
+        if (k <= row.size())
+            row.erase(row.begin() + k - 1);
     }
-    print_list(first);
+    for (auto i : row)
+        cout << i << endl;
 }
